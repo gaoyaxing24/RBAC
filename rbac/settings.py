580c062c806d7703da27399d47a3976c8976f4fd
Arthur <gaoyaxing24@163.com>
2017-04-02 12:34:41 +0800

    增加HTML的实现

--- rbac/settings.py
+++ rbac/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.static',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
